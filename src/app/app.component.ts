8b050cdefc4439aceebef0692443b7df4b332ec4
Huy Minh <minh.th12558@sinhvien.hoasen.edu.vn>
2023-02-27 22:14:47 +0700

    chat frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ChatService } from './services/chat.service';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Chat';
+
+  newRoom!: string
+  userName!: string
+  message$!: Observable<any>
+  messages: any[] = []
+  newMessage!: string
+
+  constructor(private chatService: ChatService) { }
+
+  sendMessage() {
+    let newMsg = {
+      from: this.userName,
+      msg: this.newMessage,
+      roomID: this.newRoom
+    }
+    this.chatService.sendMessage(newMsg);
+  }
+
+  joinRoom() {
+    this.message$ = this.chatService.getMessage(this.newRoom);
+    this.message$.subscribe((msg: any) => {
+      this.messages.push(msg);
+    })
+  }
 }
